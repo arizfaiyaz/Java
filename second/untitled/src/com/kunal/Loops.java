c565d8bd7ede4a3efa31dc8369cb75b2bbb28d12
arizfaiyaz <arizfaiyazwork@gmail.com>
2026-04-29 15:54:24 +0530

    some java programs added

---
+++ second/untitled/src/com/kunal/Loops.java
@@ -0,0 +1,35 @@
+package com.kunal;
+
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+
+        // Q: Print Questions from 1 to 10
+//        for (int i=1; i<=10; i++) {
+//            System.out.print(i);
+//        }
+// Q: Print the numbers from 1 to n
+/*        Scanner input = new Scanner(System.in);
+          int n = input.nextInt();
+
+           for (int i = 0; i<=n ; i++) {
+           System.out.print(i + " ");
+*/
+    }
+
+    // while loop
+//    int num = 1;
+//    while (num <= 5) {
+//        System.out.println(num);
+//        num += 1;
+//    }
+    // do while
+    int n = 1;
+    do {
+        System.out.println(n);
+        n++;
+    } while(n <=5);
+
+}
+
---
+++ second/untitled/src/com/kunal/Main.java
@@ -0,0 +1,15 @@
+package com.kunal;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Write your code here.
+        int salary = 25400;
+        if (salary > 10000) {
+            salary = salary + 2000;
+        } else {
+            salary = salary +1000;
+        }
+        System.out.println(salary);
+    }
+}
