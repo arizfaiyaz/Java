99765c6a3070175b044d1635cec2d2835fa27475
arizfaiyaz <arizfaiyazwork@gmail.com>
2026-05-04 22:32:11 +0530

    Array

---
+++ method/Mthods/src/com/ariz/First.java
@@ -0,0 +1,23 @@
+package com.ariz;
+
+public class First {
+    public static void main(String[] args) {
+/*        // Syntax
+        // datatype[] variableName = new datatype[size];
+        // store 5 role numbers
+        // int[] rnos = new int[5];
+        // or directly
+        // int[] rnos2 = {23, 45, 12, 4};
+        // int[] rnos222; //declaration of an array.
+ */
+        String[] arr = {"Ariz", "Eshal", "Ammi", "Papa"};
+
+
+    }
+}
+
+/*
+int [] arr = new int [5];
+array objects are in heap and the heap objects are not continuous.
+such that Array in java may not be continuous.
+ */
