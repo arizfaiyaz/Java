b609a490e23877853e27939819b36dbcce4873bc
arizfaiyaz <arizfaiyazwork@gmail.com>
2026-05-04 23:38:34 +0530

    Array

--- method/Mthods/src/com/ariz/ArraysInput.java
+++ method/Mthods/src/com/ariz/ArraysInput.java
@@ -1,5 +1,15 @@
 package com.ariz;
 
-public class ArraysInput {
+import java.util.Arrays;
 
+public class ArraysInput {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(arr));
+        change(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void change(int[] arr){
+        arr[0] = 99;
+    }
 }
