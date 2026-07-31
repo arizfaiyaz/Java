ae2ac1693d4609880a6615eda7e8315722e67554
arizfaiyaz <arizfaiyazwork@gmail.com>
2026-08-01 00:25:06 +0530

    lc 1295 Find Numbers with Even Number of Digits

---
+++ LinearSearch/code/src/com/kunal/EvenDigits.java
@@ -0,0 +1,38 @@
+/*
+https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+*/
+package com.kunal;
+
+public class EvenDigits {
+    public static void main(String[] args) {
+
+    }
+
+    static int findNumbers(int[] nums){
+        int count = 0;
+        for (int num : nums) {
+            if(even(num)){
+                count++;
+            }
+        }
+        return count;
+    }
+    // func to check if a number is even or not
+    static boolean even(int num){
+        int numberOfDigits = digits(num);
+        return numberOfDigits % 2 == 0;
+    }
+    // count the number of digits in a number
+    static int digits(int num){
+        if (num < 0){
+            num = num * -1;
+        }
+
+        int count = 0;
+        while (num > 0){
+            count++;
+            num /= 10;
+        }
+        return count;
+    }
+}
