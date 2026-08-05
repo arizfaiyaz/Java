8a16fdc028cd15e353f73df95a301af68c329d13
arizfaiyaz <arizfaiyazwork@gmail.com>
2026-08-05 23:17:49 +0530

    fissrt and last

--- New folder/Binary/src/com/ariz/BinarySearch.java
+++ New folder/Binary/src/com/ariz/BinarySearch.java
@@ -28,6 +28,6 @@ public class BinarySearch {
             }
 
         }
-        return start;
+        return -1;
     }
 }
---
+++ New folder/Binary/src/com/ariz/MySqrt.java
@@ -0,0 +1,11 @@
+package com.ariz;
+
+/*
+https://leetcode.com/problems/sqrtx/description/
+ */
+
+class Solution {
+    public int MySqrt(int x) {
+
+    }
+}
