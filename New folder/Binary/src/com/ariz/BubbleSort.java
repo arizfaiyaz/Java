d01cd16e259d1da876743ad785b3a67060011b8d
arizfaiyaz <arizfaiyazwork@gmail.com>
2026-08-13 21:28:28 +0530

    bubble sort

--- New folder/Binary/src/com/ariz/BubbleSort.java
+++ New folder/Binary/src/com/ariz/BubbleSort.java
@@ -2,19 +2,27 @@ package com.ariz;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arr = {};
+        int[] arr = {1, 2, 3, 4, 5};
+        bubbleSort(arr);
+        System.out.println(Arrayss.toString(arr));
     }
     static void bubbleSort(int[] arr){
+        boolean swapped;
         // run the steps n-1 times
         for (int i = 0; i < arr.length; i++){
+            swapped = false;
             // for each step, max item will come at the last respective index
-            for (int j; j < arr.length - i; j++){
-                //swap if the item is smaller
+            for (int j=1; j < arr.length - i; j++){
+                // swap if the item is smaller
                 if(arr[j] < arr[j-1]){
                     int temp = arr[j];
                     arr[j] = arr[j-1];
                     arr[j-1] = temp;
+                    swapped = true;
                 }
+            }
+            if(!swapped){
+                break;
             }
         }
     }
